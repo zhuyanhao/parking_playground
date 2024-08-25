4a7e7b263eea8ae1237fd8a2a78d2193637d4981
Yanhao Zhu <yhzhu@umich.edu>
2024-08-25 14:10:46 -0700

    Vehicle Kinematics
    Implement the vehicle kinematics in vehicle.py.

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -28,4 +28,52 @@ def test_vehicle_properties():
             front_wheel_angle_limit_rad = 1.0,
             rear_wheel_angle_limit_rad = -1.0
         )
-        
+
+def test_vehicle_kinematics_straight_line():
+    """Test kinematics of vehicle when front_wheel_angle = rear_wheel_angle."""
+    initial_state = VehicleState(
+        x_m = 1.0,
+        y_m = 5.0,
+        yaw_rad = -math.pi / 2.0
+    )
+    properties = VehicleProperties(
+        wheelbase_m = 3.0,
+        front_wheel_angle_limit_rad = math.pi,
+        rear_wheel_angle_limit_rad = math.pi
+    )
+    # drifting to the right by 5m
+    input = VehicleInput(
+        distance_moved_m = initial_state.x_m,
+        front_wheel_angle_rad = -math.pi / 2.0,
+        rear_wheel_angle_rad = -math.pi / 2.0
+    )
+    new_state = initial_state.step(properties, input)
+    epsilon = 1.0e-6
+    assert new_state.x_m == pytest.approx(0.0, epsilon)
+    assert new_state.y_m == pytest.approx(initial_state.y_m, epsilon)
+    assert new_state.yaw_rad == pytest.approx(initial_state.yaw_rad, epsilon)
+
+def test_vehicle_kinematics_front_wheel_steer_only():
+    """Test kinematics of vehicle when front wheel is steered only."""
+    initial_state = VehicleState(
+        x_m = 0.0,
+        y_m = 0.0,
+        yaw_rad = math.pi / 2.0
+    )
+    properties = VehicleProperties(
+        wheelbase_m = 3.0,
+        front_wheel_angle_limit_rad = math.pi / 4.0,
+        rear_wheel_angle_limit_rad = math.pi / 4.0
+    )
+    wheel_angle = -math.pi / 8.0
+    radius = math.fabs(properties.wheelbase_m / math.tan(wheel_angle))
+    input = VehicleInput(
+        distance_moved_m = math.pi * radius / 2.0,
+        front_wheel_angle_rad = wheel_angle,
+        rear_wheel_angle_rad = 0.0
+    )
+    new_state = initial_state.step(properties, input)
+    epsilon = 1.0e-6
+    assert new_state.x_m == pytest.approx(radius, epsilon)
+    assert new_state.y_m == pytest.approx(radius, epsilon)
+    assert new_state.yaw_rad == pytest.approx(0.0, epsilon)
--- vehicle.py
+++ vehicle.py
@@ -3,6 +3,7 @@
 from dataclasses import dataclass
 from typing import Optional, List
 import math
+import numpy as np
 
 @dataclass(frozen=True)
 class VehicleProperties:
@@ -29,20 +30,85 @@ class VehicleProperties:
         
         if (self.rear_wheel_angle_limit_rad is not None and self.rear_wheel_angle_limit_rad < 0.0):
             raise ValueError('Rear wheel angle limit must be positive.')
-        
-@dataclass(frozen=True)
-class VehicleState:
-    """Defines the state of an vehicle that supports rear wheel steering."""
-    x_m: float = 0.0  # X coordinate of the control point (center of rear axle) in world frame.
-    y_m: float = 0.0  # Y coordinate of the control point (center of rear axle) in world frame.
-    yaw_rad: float = 0.0  # Yaw angle of the vehicle in world frame.
 
 @dataclass(frozen=True)
 class VehicleInput:
     """Defines the input of an vehicle that supports rear wheel steering."""
-    distance_moved_rad: float = 0.0
+    distance_moved_m: float = 0.0
     front_wheel_angle_rad: float = 0.0
-    rear_wheel_angle_rad: Optional[float] = None
+    rear_wheel_angle_rad: float = 0.0
+
+@dataclass
+class SE2:
+    """Rigid Transformation in 2D space."""
+    x_m: float = 0.0  # X coordinate of the control point in world frame.
+    y_m: float = 0.0  # Y coordinate of the control point in world frame.
+    yaw_rad: float = 0.0  # Yaw angle in world frame.
+
+    def to_matrix(self) -> np.ndarray:
+        """Returns a matrix representation of the SE2 transformation."""
+
+        return np.array([
+            [np.cos(self.yaw_rad), -np.sin(self.yaw_rad), self.x_m],
+            [np.sin(self.yaw_rad), np.cos(self.yaw_rad), self.y_m],
+            [0, 0, 1]
+        ])
+    
+    @staticmethod
+    def create_from_matrix(matrix: np.ndarray) -> 'SE2':
+        """Creates a SE2 from a matrix representation."""
+
+        if matrix.shape!= (3, 3):
+            raise ValueError('Matrix must be 3x3.')
+
+        if not np.allclose(matrix[2], [0., 0., 1.], atol=1e-3):
+            raise ValueError('Matrix must be homogeneous.')
+        
+        return SE2(
+            x_m=matrix[0][2].item(), 
+            y_m=matrix[1][2].item(), 
+            yaw_rad=np.arctan2(matrix[1][0], matrix[0][0]).item()
+        )
+
+class VehicleState(SE2):
+    """Defines the state of an vehicle. The control point is at the center of rear axle."""
+
+    def step(self, property: VehicleProperties, input: VehicleInput) -> 'VehicleState':
+        """Forward simulate a kinematic bicycle model that supports rear wheel steering."""
+        clamped_input = VehicleInput(
+            distance_moved_m = input.distance_moved_m,
+            front_wheel_angle_rad = np.clip(input.front_wheel_angle_rad, -property.front_wheel_angle_limit_rad, property.front_wheel_angle_limit_rad).item(),
+            rear_wheel_angle_rad = np.clip(input.rear_wheel_angle_rad, -property.rear_wheel_angle_limit_rad, property.rear_wheel_angle_limit_rad).item(),
+        )
+
+        # first compute the instantaneous curvature of the vehicle
+        kappa = (math.tan(clamped_input.front_wheel_angle_rad) - math.tan(clamped_input.rear_wheel_angle_rad)) / property.wheelbase_m
+    
+        # if kappa ~= 0, then the vehicle is moving along a straight line
+        if math.isclose(kappa, 0.0, abs_tol=1e-6):
+            motion_wrt_ego = SE2(
+                x_m = input.distance_moved_m * math.cos(clamped_input.front_wheel_angle_rad),
+                y_m = input.distance_moved_m * math.sin(clamped_input.front_wheel_angle_rad),
+                yaw_rad = 0.0
+            )
+            state = SE2.create_from_matrix(self.to_matrix().dot(motion_wrt_ego.to_matrix()))
+            state.__class__ = VehicleState  # cast to child class
+            return state
+        
+        # if kappa != 0, compute vehicle motion by instantaneous rotation center
+        ego2center = SE2(
+            x_m = -math.tan(clamped_input.rear_wheel_angle_rad) / kappa,
+            y_m = 1.0 / kappa,
+            yaw_rad = -math.pi / 2.0 + clamped_input.rear_wheel_angle_rad
+        )
+        rotation = SE2(
+            x_m = 0.0,
+            y_m = 0.0,
+            yaw_rad = clamped_input.distance_moved_m * kappa
+        )
+        state = SE2.create_from_matrix(self.to_matrix().dot(ego2center.to_matrix()).dot(rotation.to_matrix()).dot(np.linalg.inv(ego2center.to_matrix())))
+        state.__class__ = VehicleState   # cast to child class
+        return state
 
 @dataclass(frozen=False)  # This class is made mutable as the children node will be updated in MCTS.
 class VehicleNode:
